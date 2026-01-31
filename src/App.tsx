e9e7148306b53a852770a81047b2c931721ed87a
rebelop7777 <rebelop7777@gmail.com>
2026-02-01 01:40:04 +0530

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div id="root" style={{ color: "#fff", textAlign: "center", padding: "40px" }}>
+      <h1>Welcome to HYDRA LED 💡</h1>
+      <p>Your cinematic automotive lighting experience is coming soon!</p>
+    </div>
+  );
+}
